d534344b684caeab6ec920cd5f7c98dbcecd7aee
suraj bahadur <surajbahadur58@gmail.com>
2020-08-11 21:44:46 +0530

    move Zeroes

---
+++ 104. Move Zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0 , j;
+        for ( j = 0; j<nums.length;j++){
+            if(nums[j]!=0){
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i]= temp;
+                i++;
+            }
+        }
+    }
+}
