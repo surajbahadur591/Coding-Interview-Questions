e62abe4863efc551de09e292f51dba1c5a78234b
suraj bahadur <surajbahadur58@gmail.com>
2020-08-12 21:59:10 +0530

    Linked List Cycle II

---
+++ 113. Linked List Cycle II.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+// Tortoise/Hare cycle detection
+
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+         ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
